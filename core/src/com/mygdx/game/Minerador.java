1cf409eb5cd1f3c45c7454fc731d86c3305bd9e0
Vitoria <vicolmoraes@gmail.com>
2020-07-09 23:13:53 -0300

    Commit inicial de codigo

---
+++ core/src/com/mygdx/game/Minerador.java
@@ -0,0 +1,24 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.Screens.PlayScreen;
+
+public class Minerador extends Game {
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+}
